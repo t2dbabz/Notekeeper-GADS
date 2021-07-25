4c206c7c9884c39ef4e97cff7bde37bd5351b463
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-07-25 10:21:05 +0100

    Understanding Drawer Navigation

---
+++ app/src/main/java/com/jwhh/notekeeper/ItemsActivity.kt
@@ -0,0 +1,74 @@
+package com.jwhh.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jwhh.notekeeper.databinding.ActivityItemsBinding
+
+class ItemsActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityItemsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityItemsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarItems.toolbar)
+
+        binding.appBarItems.fab.setOnClickListener {
+            startActivity(Intent(this, NoteActivity::class.java))
+        }
+
+      val listItem =  binding.appBarItems.noteListContent.listItems
+
+        listItem.layoutManager = LinearLayoutManager(this)
+        listItem.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+
+
+        val toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout, binding.appBarItems.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.bringToFront()
+
+
+
+
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.items, menu)
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.appBarItems.noteListContent.listItems.adapter?.notifyDataSetChanged()
+    }
+
+}
