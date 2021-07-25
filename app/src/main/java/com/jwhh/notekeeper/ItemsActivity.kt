3afaddbf291a8906bd2e2f9ed1a14d782f2bc411
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-07-25 11:46:28 +0100

    Implementing Drawer Navigation Behaviour

--- app/src/main/java/com/jwhh/notekeeper/ItemsActivity.kt
+++ app/src/main/java/com/jwhh/notekeeper/ItemsActivity.kt
@@ -3,22 +3,34 @@ package com.jwhh.notekeeper
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jwhh.notekeeper.databinding.ActivityItemsBinding
 
-class ItemsActivity : AppCompatActivity() {
 
+
+class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private val noteLayoutManager by lazy {
+        LinearLayoutManager(this)
+    }
+    private val noteRecyclerAdapter by lazy {
+        NoteRecyclerAdapter(this, DataManager.notes)
+    }
+
+    private val coursesLayoutManager by lazy {
+        GridLayoutManager(this, 2)
+    }
+
+    private val courseRecyclerAdapter by lazy {
+        CourseRecyclerAdapter(this, DataManager.courses.values.toList())
+    }
 
     private lateinit var binding: ActivityItemsBinding
 
@@ -36,8 +48,7 @@ class ItemsActivity : AppCompatActivity() {
 
       val listItem =  binding.appBarItems.noteListContent.listItems
 
-        listItem.layoutManager = LinearLayoutManager(this)
-        listItem.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+        displayNotes(listItem)
 
 
         val toggle = ActionBarDrawerToggle(
@@ -45,11 +56,23 @@ class ItemsActivity : AppCompatActivity() {
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        binding.navView.bringToFront()
+        binding.navView.setNavigationItemSelectedListener(this)
 
 
+    }
 
+    private fun displayNotes(listItem: RecyclerView) {
+        listItem.layoutManager = noteLayoutManager
+        listItem.adapter = noteRecyclerAdapter
 
+        binding.navView.menu.findItem(R.id.nav_notes).isChecked = true
+    }
+
+    private fun displayCourses(listItem: RecyclerView){
+        listItem.layoutManager = coursesLayoutManager
+        listItem.adapter = courseRecyclerAdapter
+
+        binding.navView.menu.findItem(R.id.nav_courses).isChecked = true
     }
 
     override fun onBackPressed() {
@@ -69,6 +92,36 @@ class ItemsActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         binding.appBarItems.noteListContent.listItems.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+
+            R.id.nav_notes -> {
+                displayNotes(binding.appBarItems.noteListContent.listItems)
+            }
+
+            R.id.nav_courses ->{
+                displayCourses(binding.appBarItems.noteListContent.listItems)
+            }
+
+            R.id.nav_share -> {
+                handleSelection("Don't think you've shared enough")
+            }
+            R.id.nav_send -> {
+                handleSelection("Send")
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+
+
+    }
+
+    private fun handleSelection(message: String) {
+        Snackbar.make(binding.appBarItems.fab, message, Snackbar.LENGTH_LONG).show()
     }
 
 }
